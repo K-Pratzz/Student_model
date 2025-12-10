d42369e79f0911f8d967ed58a16c62fb75eabd55
Kumari Pratibha <kp939331@gmail.com>
2025-12-10 06:41:36 +0530

    added more

--- app.py
+++ app.py
@@ -5,9 +5,11 @@ model=joblib.load("Student_model.pkl")
 st.title("Student Score Predictor")
 
 # Inputs from user
-feature1 = st.number_input("Enter Study Time:", min_value=0.0)
-feature2 = st.number_input("Enter Travel time:", min_value=0.0)
-
+st.header("HOURS OF STUDY")
+feature1 = st.number_input("Enter Study Time:", min_value=0.0,max_value=20.0)
+st.header("Hours of travel")
+feature2 = st.number_input("Enter Travel time:", min_value=0.0,max_value=20.0)
+st.write("wait..................")
 # Predict button
 if st.button("Predict"):
     input_data = np.array([[feature1, feature2]])
